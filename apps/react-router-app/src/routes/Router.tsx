71c5a52079ffa584f9853a0c24008141822326b5
Joon Sunn Foo <joonsunn.foo@gmail.com>
2025-05-01 22:10:26 +0800

    feat: set up posts route

--- apps/react-router-app/src/routes/Router.tsx
+++ apps/react-router-app/src/routes/Router.tsx
@@ -1,16 +1,32 @@
 import { createBrowserRouter } from "react-router";
 import Home from "../pages/home/Home";
 import BaseLayout from "../layouts/BaseLayout";
+import Post from "../pages/posts/Post";
+import Posts from "../pages/posts/Posts";
 
 export const router = createBrowserRouter([
-    {
-      path: "/",
-      Component: BaseLayout,
-      children: [
-        {
+  {
+    path: "/",
+    Component: BaseLayout,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: "posts",
+
+        children: [
+          {
             index: true,
-            element: <Home />,
-        }
-      ]
-    },
-  ]);
+            element: <Posts />,
+          },
+          {
+            path: ":postId",
+            element: <Post />,
+          },
+        ],
+      },
+    ],
+  },
+]);
